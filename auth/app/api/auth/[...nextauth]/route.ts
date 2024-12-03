735d87e8e508a234dde4b81b4ce7f4e8d28ddcc3
Sabbir Tanvir <sabbirmahmudtanvir@gmail.com>
2024-12-03 23:11:18 +0600

    providor

--- auth/app/api/auth/[...nextauth]/route.ts
+++ auth/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,47 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-        name: 'Credentials',
-        credentials: {
-          username: { label: 'email', type: 'text', placeholder: '' },
-          password: { label: 'password', type: 'password', placeholder: '' },
-        },
-        async authorize(credentials: any) {
-            console.log(credentials);
-            
-            return {
-                id: "user1",
-                name: "sabbir",
-                email: "sabbir@gmail.com"
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: { label: 'email', type: 'text', placeholder: '' },
+                password: { label: 'password', type: 'password', placeholder: '' },
+            },
+            async authorize(credentials: any) {
+                console.log(credentials);
 
-            };
-        },
-      })
-  ],
-  secret: process.env.NEXTAUTH_SECRET
+                return {
+                    id: "user1",
+                    name: "sabbir",
+                    email: "sabbir@gmail.com"
+                };
+            },
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+          }),
+
+          GitHubProvider({
+            clientId: process.env.GITHUB_ID || "",
+            clientSecret: process.env.GITHUB_SECRET || "",
+          })
+    ],
+    
+    secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        signIn: ({ user }) => {
+            if (user.email == "saba@gmail.com") {
+                return false;
+            }
+            return true;
+
+        }
+    }
 })
 
 export { handler as GET, handler as POST }
